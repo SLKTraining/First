6d1e478f8281c2a5c529e2951d1c20c7834f42ae
Sudarshan Somayaji K <sudarshan.k@SLKSOFT.com>
2018-06-29 18:29:47 +0530

    changed file

--- src/main/java/com/khoubyari/example/api/rest/HotelController.java
+++ src/main/java/com/khoubyari/example/api/rest/HotelController.java
@@ -101,6 +101,6 @@ public class HotelController extends AbstractRestHandler {
 	public void testRet(){
 		String hello;
 		
-		hello = "Hi.. Sudarshan..";
+		hello = "Hi.. Sudarshan.. from new branch SUDFOR18";
 	}
 }
